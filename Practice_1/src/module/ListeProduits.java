9552f3d37bf5c150f9bdcaec6a31e7207f48397d
Amr Seoudy <amrsoudy@github.com>
2018-02-21 09:59:17 -0500

    new_copy of practice

---
+++ Practice_1/src/module/ListeProduits.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package module;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 1795162
+ */
+public class ListeProduits extends ArrayList<Produit> {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
