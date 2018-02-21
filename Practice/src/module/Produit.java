5c77e48c4b36f25a9b8268a337e09535a3bfd566
Amr Seoudy <amrsoudy@github.com>
2018-02-21 09:58:30 -0500

    all that errours

---
+++ Practice/src/module/Produit.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package module;
+
+/**
+ *
+ * @author 1795162
+ */
+public class Produit {
+    
+    String nom ;
+    double prix ;
+    int quantity; 
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return ""+ nom + ","+ prix + ","+ quantity;
+    }
+    
+    
+    
+    
+}
