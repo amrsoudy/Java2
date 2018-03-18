1b49568d82cafa5ba01855f835f5a28053dbb299
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2018-03-18 16:12:38 -0400

    project

---
+++ projet_ResidenceDuCalme/src/control/AppCtr.java
@@ -0,0 +1,30 @@
+package control;
+/**
+ *
+ * @author Marc A. & AMR
+ */
+import javax.swing.JFrame;
+import vue.FenContrat;
+import vue.FenLogin;
+
+public class AppCtr {
+
+    public static void main(String[] args) {
+        int width = 400, height = 200;
+        //Creation de l'Objet GUI
+        //FenLogin objGui = new FenLogin();
+        FenLogin objGui = new FenLogin();
+        //Execution de la methode Run
+        objGui.run();
+        //Titre du Frame
+        objGui.setTitle("Résidence du calme");
+        // Donner une taille de la frame pour  affichage
+        objGui.setSize(width, height);
+        //Afficher la frame
+
+        objGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        objGui.setLocationRelativeTo(null);
+        objGui.setResizable(false);
+        objGui.setVisible(true);
+    }
+}
